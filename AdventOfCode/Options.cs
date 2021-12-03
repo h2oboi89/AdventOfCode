2030f00cfd3d5c8a1da48b7b66f32cea6acc2613
Hotel2Oscar <waters.daniel.c@gmail.com>
2021-12-03 17:19:18 -0500

    Command line options and more solver methods to support

---
+++ AdventOfCode/Options.cs
@@ -0,0 +1,15 @@
+﻿using CommandLine;
+
+namespace AdventOfCode;
+
+public class Options
+{
+    [Option('y', "year", Required = false)]
+    public int Year { get; set; } = -1;
+
+    [Option("all", Required = false)]
+    public bool All { get; set; } = false;
+
+    [Value(0, Required = false)]
+    public IEnumerable<int> Days { get; set; }
+}
