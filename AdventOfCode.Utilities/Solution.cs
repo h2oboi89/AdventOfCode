1d9858aae7eb82e7c3d0942c159f2aa7c29e952e
Hotel2Oscar <waters.daniel.c@gmail.com>
2021-12-06 12:52:56 -0500

    Forgot to commit class for last PR

---
+++ AdventOfCode.Utilities/Solution.cs
@@ -0,0 +1,41 @@
+﻿namespace AdventOfCode.Utilities;
+
+public class Solution
+{
+    public readonly int Year;
+    public readonly int Day;
+    public readonly string TypeName;
+    public TimeSpan Construction = TimeSpan.Zero;
+    public TimeSpan RunTime = TimeSpan.Zero;
+    public List<(string name, bool passed, TimeSpan duration)> Tests = new();
+    public List<(string name, string result, TimeSpan duration)> Parts = new();
+
+    public Solution(int year, int day, string typeName)
+    {
+        Year = year;
+        Day = day;
+        TypeName = typeName;
+    }
+
+    public string Name => $"{Year}.{TypeName}";
+
+    public TimeSpan ExecutionTime
+    {
+        get
+        {
+            var result = Construction;
+
+            foreach (var (_, __, duration) in Tests)
+            {
+                result += duration;
+            }
+
+            foreach (var (_, __, duration) in Parts)
+            {
+                result += duration;
+            }
+
+            return result;
+        }
+    }
+}
