2c8fe63c19070e1f2752a7d13469371c8b7bf9c5
Hotel2Oscar <waters.daniel.c@gmail.com>
2021-12-14 14:43:30 -0500

    Adding 2021.08 files

---
+++ AdventOfCode/2021/Day_08.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2021;
+
+internal class Day_08 : BaseDay
+{
+    public Day_08(string inputFile)
+    {
+
+    }
+}
