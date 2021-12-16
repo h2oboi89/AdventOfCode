f6359482b6b1a7eaef4ad110c7ecae260925678f
Hotel2Oscar <waters.daniel.c@gmail.com>
2021-12-15 22:53:41 -0500

    2021.12.2

--- AdventOfCode/2021/Day_12.cs
+++ AdventOfCode/2021/Day_12.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AdventOfCode._2021;
+﻿namespace AdventOfCode._2021;
 
 internal class Day_12 : BaseDay
 {
@@ -12,6 +6,7 @@ internal class Day_12 : BaseDay
     private readonly List<Dictionary<string, Node>> testInputs = new();
 
     private const string START = "start", END = "end";
+    private const int UNLIMITED = -1;
 
     public Day_12(string inputFile)
     {
@@ -119,6 +114,91 @@ internal class Day_12 : BaseDay
         return clone;
     }
 
+    private static List<Dictionary<string, int>> GenerateVisitLimits(Dictionary<string, Node> nodes)
+    {
+        var visitLimits = new List<Dictionary<string, int>>();
+
+        var baseLimits = new Dictionary<string, int>();
+
+        foreach (var kvp in nodes)
+        {
+            baseLimits.Add(kvp.Key, 1);
+
+            if (nodes[kvp.Key].Size == Size.Large)
+            {
+                baseLimits[kvp.Key] = UNLIMITED;
+            }
+        }
+
+        foreach (var kvp in nodes.Where(kvp => kvp.Value.Size == Size.Small
+             && kvp.Value.Id != START && kvp.Value.Id != END))
+        {
+            var clone = new Dictionary<string, int>(baseLimits);
+
+            clone[kvp.Key]++;
+
+            visitLimits.Add(clone);
+        }
+
+        return visitLimits;
+    }
+
+    private static List<List<Node>> GeneratePaths(Dictionary<string, Node> nodes, IEnumerable<Dictionary<string, int>> visitLimits)
+    {
+        var paths = new Dictionary<string, List<Node>>();
+
+        foreach (var visitLimit in visitLimits)
+        {
+            var p = GeneratePaths(nodes, visitLimit);
+
+            foreach (var path in p)
+            {
+                paths[string.Join("->", path.Select(n => n.Id))] = path;
+            }
+        }
+
+        return paths.Select(kvp => kvp.Value).ToList();
+
+    }
+
+    private static List<List<Node>> GeneratePaths(Dictionary<string, Node> nodes, Dictionary<string, int> visitLimits)
+    {
+        var paths = new List<List<Node>>();
+
+        var processing = new Queue<(List<Node>, Node, Dictionary<string, int>)>();
+
+        processing.Enqueue((new List<Node>(), nodes[START], visitLimits));
+
+        while (processing.Any())
+        {
+            var (path, current, limits) = processing.Dequeue();
+
+            if (path.Contains(current) && limits[current.Id] == 0)
+            {
+                continue;
+            }
+
+            path.Add(current);
+            if (limits[current.Id] != UNLIMITED)
+            {
+                limits[current.Id]--;
+            }
+
+            if (current.Id == END)
+            {
+                paths.Add(path);
+                continue;
+            }
+
+            foreach (var node in current.Connections)
+            {
+                processing.Enqueue((Clone(path), node, new Dictionary<string, int>(limits)));
+            }
+        }
+
+        return paths;
+    }
+
     private static List<List<Node>> GeneratePaths(Dictionary<string, Node> nodes)
     {
         var paths = new List<List<Node>>();
@@ -158,9 +238,21 @@ internal class Day_12 : BaseDay
     {
         var testInputAndResults = testInputs.Zip(new List<object> { 10, 19, 226 }, (input, expected) => (input, expected));
 
-        return ExecuteTests(testInputAndResults, (i) => GeneratePaths(i).Count);
+        return ExecuteTests(testInputAndResults, (nodes) => GeneratePaths(nodes).Count);
+    }
+
+    [Test]
+    public TestResult Test2()
+    {
+        var testInputAndResults = testInputs.Zip(new List<object> { 36, 103, 3509 }, (input, expected) => (input, expected));
+
+        return ExecuteTests(testInputAndResults, (nodes) =>
+            GeneratePaths(nodes, GenerateVisitLimits(nodes)).Count);
     }
 
     [Part]
     public string Solve1() => $"{GeneratePaths(input).Count}";
+
+    [Part]
+    public string Solve2() => $"{GeneratePaths(input, GenerateVisitLimits(input)).Count}";
 }
