f7b66002815e275a6fea6f28ef74d0d7c14d8a6b
Hotel2Oscar <waters.daniel.c@gmail.com>
2021-12-17 14:15:57 -0500

    2021.17 files

---
+++ AdventOfCode/2021/Day_17.cs
@@ -0,0 +1,15 @@
+﻿namespace AdventOfCode._2021;
+
+internal class Day_17 : BaseDay
+{
+    public Day_17(string inputFile)
+    {
+
+    }
+
+    [Test]
+    public TestResult Test1() => new TestResult();
+
+    [Part]
+    public string Solve1() => string.Empty;
+}
