4371421ee7a918311f2cb6ebca305a42e20ec2c4
Hotel2Oscar <waters.daniel.c@gmail.com>
2021-12-19 16:31:03 -0500

    added all helper methods I think i will need to Scanner class

--- AdventOfCode/2021/Day_19.cs
+++ AdventOfCode/2021/Day_19.cs
@@ -1,22 +1,5 @@
 ﻿using AdventOfCode.Common;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-
-/*
-    foreach of 3 axis for base matrix:
-        foreach of 3 axis for test matrix:
-            foreach of 4 rotations of test matrix:
-                // TODO: try to match at least 12 points and add to candidate group
-                // this will be 2D translation step
-    
-    foreach candidate in candidates:
-        // TODO: unflatten and slide along axis until 12 points match or out of range
-        // TODO: combine matrices if match and restart until only 1 matrix left
- */
 
 namespace AdventOfCode._2021;
 
@@ -84,7 +67,136 @@ internal class Day_19 : BaseDay
             Id = id;
             _points.AddRange(points);
         }
+
+        private static Exception InvalidAxis(Axis axis) => new Exception($"Invalid axis {axis}");
+
+        public Scanner Flatten(Axis axis)
+        {
+            var points = new List<Point3D>();
+
+            foreach (var point in Points)
+            {
+                points.Add(axis switch
+                {
+                    Axis.X => new Point3D(0, point.Y, point.Z),
+                    Axis.Y => new Point3D(point.X, 0, point.Z),
+                    Axis.Z => new Point3D(point.X, point.Y, 0),
+                    _ => throw InvalidAxis(axis)
+                });
+            }
+
+            return new Scanner(Id, points);
+        }
+
+        public Scanner Rotate(Axis axis)
+        {
+            var points = new List<Point3D>();
+
+            foreach (var point in Points)
+            {
+                points.Add(axis switch
+                {
+                    Axis.X => new Point3D(point.X, point.Z, -point.Y),
+                    Axis.Y => new Point3D(-point.Z, point.Y, point.X),
+                    Axis.Z => new Point3D(point.Y, -point.X, point.Z),
+                    _ => throw InvalidAxis(axis)
+                });
+            }
+
+            return new Scanner(Id, points);
+        }
+
+        public Scanner Translate(Axis axis, int distance)
+        {
+            var points = new List<Point3D>();
+
+            foreach (var point in Points)
+            {
+                points.Add(axis switch
+                {
+                    Axis.X => new Point3D(point.X + distance, point.Y, point.Z),
+                    Axis.Y => new Point3D(point.X, point.Y + distance, point.Z),
+                    Axis.Z => new Point3D(point.X, point.Y, point.Z + distance),
+                    _ => throw InvalidAxis(axis)
+                });
+            }
+
+            return new Scanner(Id, points);
+        }
+
+        public (int min, int max) Range(Axis axis)
+        {
+            var min = int.MaxValue; var max = int.MinValue;
+
+            static (int, int) Check(int a, int min, int max)
+            {
+                if (a < min) min = a;
+                if (a > max) max = a;
+
+                return (min, max);
+            }
+
+            foreach (var point in Points)
+            {
+
+                (min, max) = axis switch
+                {
+                    Axis.X => Check(point.X, min, max),
+                    Axis.Y => Check(point.Y, min, max),
+                    Axis.Z => Check(point.Z, min, max),
+                    _ => throw InvalidAxis(axis)
+                };
+            }
+
+            return (min, max);
+        }
+
+        public Scanner Merge(Scanner other)
+        {
+            var points = new List<Point3D>();
+
+            points.AddRange(Points);
+
+            foreach (var point in other.Points)
+            {
+                if (!points.Contains(point))
+                {
+                    points.Add(point);
+                }
+            }
+
+            return new Scanner($"{Id}-{other.Id}", points);
+        }
+
+        public int Compare(Scanner other)
+        {
+            var matches = 0;
+
+            foreach (var point in Points)
+            {
+                if (other.Points.Contains(point))
+                {
+                    matches++;
+                }
+            }
+
+            return matches;
+        }
+
+        public override string ToString() => $"{Id} ({string.Join(", ", Points)})";
     }
+
+    /*
+        foreach of 3 axis for base matrix:
+            foreach of 3 axis for test matrix:
+                foreach of 4 rotations of test matrix:
+                    // TODO: try to match at least 12 points and add to candidate group
+                    // this will be 2D translation step
+    
+        foreach candidate in candidates:
+            // TODO: unflatten and slide along axis until 12 points match or out of range
+            // TODO: combine matrices if match and restart until only 1 matrix left
+    */
 
     [DayTest]
     public TestResult ParseTest()
