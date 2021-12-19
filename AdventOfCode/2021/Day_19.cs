aa1e8ccbefd7f953062b595ac9506383414868b2
Hotel2Oscar <waters.daniel.c@gmail.com>
2021-12-19 15:42:31 -0500

    input parsing for 2021.19

--- AdventOfCode/2021/Day_19.cs
+++ AdventOfCode/2021/Day_19.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using AdventOfCode.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 /*
@@ -20,5 +22,79 @@ namespace AdventOfCode._2021;
 
 internal class Day_19 : BaseDay
 {
-    public Day_19(string inputFile) { }
+    private readonly List<Scanner> partScanners = new();
+    private readonly List<Scanner> testScanners = new();
+
+    public Day_19(string inputFile)
+    {
+        var scanners = new List<Scanner>();
+        var id = string.Empty;
+        var points = new List<Point3D>();
+
+        var idRegex = new Regex(@"--- scanner (?<id>\d+) ---");
+
+        foreach (var line in File.ReadLines(inputFile))
+        {
+            if (line.StartsWith("###"))
+            {
+                testScanners.AddRange(scanners);
+                scanners.Clear();
+                continue;
+            }
+
+            if (line.StartsWith("!!!"))
+            {
+                partScanners.AddRange(scanners);
+                scanners.Clear();
+                continue;
+            }
+
+            if (line.StartsWith("---"))
+            {
+                id = idRegex.Match(line).Groups["id"].Value;
+                continue;
+            }
+
+            if (line.Length == 0)
+            {
+                scanners.Add(new Scanner(id, points));
+                points.Clear();
+                continue;
+            }
+
+            // default line is coordinate triplet
+            var coords = line.Split(',').Select(int.Parse).ToArray();
+            points.Add(new Point3D(coords[0], coords[1], coords[2]));
+        }
+    }
+
+    private class Scanner
+    {
+        private readonly List<Point3D> _points = new();
+
+        public IEnumerable<Point3D> Points
+        {
+            get { foreach (var point in _points) yield return point; }
+        }
+
+        public readonly string Id;
+
+        public Scanner(string id, IEnumerable<Point3D> points)
+        {
+            Id = id;
+            _points.AddRange(points);
+        }
+    }
+
+    [Test]
+    public TestResult ParseTest()
+    {
+        var testValues = new List<(int, int)>()
+        {
+            (testScanners.Count, 5),
+            (partScanners.Count, 40)
+        };
+
+        return ExecuteTests(testValues, i => i);
+    }
 }
