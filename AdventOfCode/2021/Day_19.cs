eebee422df3e68b263dd7aa7523ee8b44d832cf4
Hotel2Oscar <waters.daniel.c@gmail.com>
2021-12-20 12:49:00 -0500

    2021.19 solution
    Clocks in at just over 2 minutes in release mode and maxes out CPU for the entire time

--- AdventOfCode/2021/Day_19.cs
+++ AdventOfCode/2021/Day_19.cs
@@ -5,12 +5,12 @@ namespace AdventOfCode._2021;
 
 internal class Day_19 : BaseDay
 {
-    private readonly List<Scanner> partScanners = new();
-    private readonly List<Scanner> testScanners = new();
+    private readonly List<Field> partsFields = new();
+    private readonly List<Field> testFields = new();
 
     public Day_19(string inputFile)
     {
-        var scanners = new List<Scanner>();
+        var fields = new List<Field>();
         var id = string.Empty;
         var points = new List<Point3D>();
 
@@ -20,15 +20,15 @@ internal class Day_19 : BaseDay
         {
             if (line.StartsWith("###"))
             {
-                testScanners.AddRange(scanners);
-                scanners.Clear();
+                testFields.AddRange(fields);
+                fields.Clear();
                 continue;
             }
 
             if (line.StartsWith("!!!"))
             {
-                partScanners.AddRange(scanners);
-                scanners.Clear();
+                partsFields.AddRange(fields);
+                fields.Clear();
                 continue;
             }
 
@@ -40,7 +40,7 @@ internal class Day_19 : BaseDay
 
             if (line.Length == 0)
             {
-                scanners.Add(new Scanner(id, points));
+                fields.Add(new Field(id, points));
                 points.Clear();
                 continue;
             }
@@ -51,41 +51,47 @@ internal class Day_19 : BaseDay
         }
     }
 
-    private class Scanner
+    private class Field
     {
-        private readonly List<Point3D> _points = new();
+        private readonly List<Point3D> _scanners = new() { new Point3D(0, 0, 0) };
+        private readonly List<Point3D> _beacons = new();
 
-        public IEnumerable<Point3D> Points
+        public IEnumerable<Point3D> Beacons
         {
-            get { foreach (var point in _points) yield return point; }
+            get { foreach (var beacon in _beacons) yield return beacon; }
+        }
+
+        public IEnumerable<Point3D> Scanners
+        {
+            get { foreach (var scanner in _scanners) yield return scanner; }
         }
 
         public readonly string Id;
 
-        public Scanner(string id, IEnumerable<Point3D> points)
+        public Field(string id, IEnumerable<Point3D> beacons)
         {
             Id = id;
-            _points.AddRange(points);
+            _beacons.AddRange(beacons);
+        }
+
+        private Field(string id, IEnumerable<Point3D> beacons, IEnumerable<Point3D> scanners) : this(id, beacons)
+        {
+            _scanners = new(scanners);
         }
 
         private static Exception InvalidAxis(Axis axis) => new($"Invalid axis {axis}");
 
-        private IEnumerable<Point3D> ForEachPoint(Func<Point3D, Point3D> transformFunc)
+        private static IEnumerable<Point3D> ForEach(IEnumerable<Point3D> points, Func<Point3D, Point3D> transformFunc)
         {
-            foreach (var point in Points)
+            foreach (var point in points)
             {
                 yield return transformFunc(point);
             }
         }
 
-        /// <summary>
-        /// Rotates <see cref="Scanner.Points"/> clockwise by 90 degrees around <paramref name="axis"/>.
-        /// </summary>
-        /// <param name="axis"><see cref="Axis"/> to rotate around.</param>
-        /// <returns>New <see cref="Scanner"/> with <see cref="Scanner.Points"/> rotated by 90 degrees around <paramref name="axis"/></returns>
-        public Scanner Rotate(Axis axis)
+        public Field Rotate(Axis axis)
         {
-            var points = ForEachPoint(point => axis switch
+            IEnumerable<Point3D> Rotate(IEnumerable<Point3D> points) => ForEach(points, point => axis switch
             {
                 Axis.X => new Point3D(point.X, point.Z, -point.Y),
                 Axis.Y => new Point3D(-point.Z, point.Y, point.X),
@@ -93,69 +99,29 @@ internal class Day_19 : BaseDay
                 _ => throw InvalidAxis(axis)
             });
 
-            return new Scanner(Id, points);
+            return new Field(Id, Rotate(Beacons), Rotate(Scanners));
         }
 
-        /// <summary>
-        /// Translates <see cref="Scanner.Points"/> by specified distances.
-        /// </summary>
-        /// <param name="dx"><see cref="Axis.X"/> adjustment.</param>
-        /// <param name="dy"><see cref="Axis.Y"/> adjustment.</param>
-        /// <param name="dz"><see cref="Axis.Z"/> adjustment.</param>
-        /// <returns>New <see cref="Scanner"/> with <see cref="Scanner.Points"/> adjusted by the specified amounts.</returns>
-        public Scanner Translate(int dx, int dy, int dz) =>
-            new(Id, ForEachPoint(point => new Point3D(point.X + dx, point.Y + dy, point.Z + dz)));
-
-        public (int min, int max) Range(Axis axis)
+        public Field Translate(Point3D translation)
         {
-            var min = int.MaxValue; var max = int.MinValue;
+            IEnumerable<Point3D> Translate(IEnumerable<Point3D> points, Point3D translation) =>
+                ForEach(points, point => new Point3D(point.X + translation.X, point.Y + translation.Y, point.Z + translation.Z));
 
-            static (int, int) Check(int a, int min, int max)
-            {
-                if (a < min) min = a;
-                if (a > max) max = a;
-
-                return (min, max);
-            }
-
-            foreach (var point in Points)
-            {
-                (min, max) = axis switch
-                {
-                    Axis.X => Check(point.X, min, max),
-                    Axis.Y => Check(point.Y, min, max),
-                    Axis.Z => Check(point.Z, min, max),
-                    _ => throw InvalidAxis(axis)
-                };
-            }
-
-            return (min, max);
+            return new Field(Id, Translate(Beacons, translation), Translate(Scanners, translation));
         }
 
-        public Scanner Merge(Scanner other)
-        {
-            var points = new List<Point3D>();
+        public Field Merge(Field other) =>
+            new($"{Id}-{other.Id}", Beacons.Concat(other.Beacons).Distinct(), Scanners.Concat(other.Scanners).Distinct());
 
-            points.AddRange(Points);
+        public Field Clone() => new(Id, Beacons, Scanners);
 
-            foreach (var point in other.Points)
-            {
-                if (!points.Contains(point))
-                {
-                    points.Add(point);
-                }
-            }
-
-            return new Scanner($"{Id}-{other.Id}", points);
-        }
-
-        public int Compare(Scanner other)
+        public int Compare(Field other)
         {
             var matches = 0;
 
-            foreach (var point in Points)
+            foreach (var beacon in Beacons)
             {
-                if (other.Points.Contains(point))
+                if (other.Beacons.Contains(beacon))
                 {
                     matches++;
                 }
@@ -164,21 +130,214 @@ internal class Day_19 : BaseDay
             return matches;
         }
 
-        public override string ToString() => $"{Id} ({string.Join(", ", Points)})";
-
-        public static Scanner? Match(Scanner a, Scanner b)
+        public override bool Equals(object? obj)
         {
-            Scanner? result = null;
+            if (obj == null) return false;
 
-            /*
-             * 1. Create all permutations
-             * 2. Eliminate duplicates
-             * 3. for (n * m) scanners set origin and check matches
-             *      if (matches >= 12) calculate offset and merge
-             */
+            if (obj is not Field other) return false;
 
-            return result;
+            if (Id != other.Id) return false;
+
+            return Scanners.SequenceEqual(other.Scanners) && Beacons.SequenceEqual(other.Beacons);
         }
+
+        public override int GetHashCode()
+        {
+            var hash = _beacons.Count;
+
+            foreach (var scanner in Scanners)
+            {
+                hash ^= scanner.GetHashCode();
+            }
+
+            foreach (var beacon in Beacons)
+            {
+                hash ^= beacon.GetHashCode();
+            }
+
+            return hash;
+        }
+
+        public override string ToString() => $"{Id} [{Scanners.Count()}] ({string.Join(", ", Beacons)})";
+
+        public Field? Align(Field other)
+        {
+            static IEnumerable<Field> GenerateRotations(Field field)
+            {
+                var permutations = new List<Field>();
+
+                var temp = field.Clone();
+
+                for (var x = 0; x < 4; x++)
+                {
+                    for (var y = 0; y < 4; y++)
+                    {
+                        for (var z = 0; z < 4; z++)
+                        {
+                            if (!permutations.Contains(temp)) permutations.Add(temp);
+
+                            temp = temp.Rotate(Axis.Z);
+                        }
+
+                        temp = temp.Rotate(Axis.Y);
+                    }
+
+                    temp = temp.Rotate(Axis.X);
+                }
+
+                return permutations;
+            }
+
+            static IEnumerable<Point3D> GenerateTranslations(Field field)
+            {
+                foreach (var beacon in field.Beacons)
+                {
+                    yield return -beacon;
+                }
+            }
+
+            var translations = GenerateTranslations(this);
+
+            foreach (var rotation in GenerateRotations(other))
+            {
+                var otherTranslations = GenerateTranslations(rotation);
+
+                foreach (var (aT, bT) in translations.CartesianProduct(otherTranslations))
+                {
+                    var temp = rotation.Translate(bT);
+
+                    var count = Translate(aT).Compare(temp);
+
+                    if (count >= 12)
+                    {
+                        return temp.Translate(-aT);
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        public static IEnumerable<Field> Align(IEnumerable<Field> fields)
+        {
+            var tasks = new List<Task<List<Field>>>();
+
+            foreach (var (a, b) in fields.UniqueProduct())
+            {
+                tasks.Add(Task.Run(() =>
+                {
+                    var c = a.Align(b);
+
+                    if (c != null)
+                    {
+                        return new List<Field>() { a, c };
+                    }
+
+                    return new();
+                }));
+            }
+
+            Task.WaitAll(tasks.ToArray());
+
+            var alignedFields = tasks.Where(t => t.Result.Any()).Select(t => t.Result).ToList();
+
+            static (Field a, int groupIndex, int scannerIndex)? FindMatch(Field a, List<List<Field>> fields)
+            {
+                for (var i = 0; i < fields.Count; i++)
+                {
+                    for (var j = 0; j < fields[i].Count; j++)
+                    {
+                        if (a.Id == fields[i][j].Id)
+                        {
+                            return (a, i, j);
+                        }
+                    }
+                }
+
+                return null;
+            }
+
+            while (alignedFields.Count > 1)
+            {
+                var rootGroup = alignedFields.First();
+
+                alignedFields.RemoveAt(0);
+
+                var success = false;
+                foreach (var aligned in rootGroup)
+                {
+                    var match = FindMatch(aligned, alignedFields);
+
+                    if (match != null)
+                    {
+                        var (a, groupIndex, scannerIndex) = match.Value;
+
+                        var nextGroup = alignedFields[groupIndex];
+
+                        alignedFields.RemoveAt(groupIndex);
+                        nextGroup.RemoveAt(scannerIndex);
+
+                        foreach (var b in nextGroup)
+                        {
+                            var c = a.Align(b);
+
+                            if (c != null) rootGroup.Add(c);
+                        }
+
+                        success = true;
+                        break;
+                    }
+                }
+
+                if (success)
+                {
+                    alignedFields.Insert(0, rootGroup);
+                }
+                else
+                {
+                    alignedFields.Add(rootGroup);
+                }
+            }
+
+            return alignedFields.First();
+        }
+
+        public static Field Merge(IEnumerable<Field> fields)
+        {
+            var root = fields.First();
+
+            foreach (var field in fields.Skip(1))
+            {
+                root = root.Merge(field);
+            }
+
+            return root;
+        }
+    }
+
+    private static int LargestDistance(Field field)
+    {
+        var max = int.MinValue;
+
+        foreach (var (a, b) in field.Scanners.UniqueProduct())
+        {
+            var dx = Math.Abs(a.X - b.X);
+            var dy = Math.Abs(a.Y - b.Y);
+            var dz = Math.Abs(a.Z - b.Z);
+
+            var manhattanDistance = dx + dy + dz;
+
+            if (manhattanDistance > max) max = manhattanDistance;
+        }
+
+        return max;
+    }
+
+    private static (int beacons, int maxDistance) Merge(IEnumerable<Field> fields)
+    {
+        var merged = Field.Merge(Field.Align(fields));
+
+        return (merged.Beacons.Count(), LargestDistance(merged));
     }
 
     [DayTest]
@@ -186,10 +345,21 @@ internal class Day_19 : BaseDay
     {
         var testValues = new List<(int, int)>()
         {
-            (testScanners.Count, 5),
-            (partScanners.Count, 40)
+            (testFields.Count, 5),
+            (partsFields.Count, 40)
         };
 
         return ExecuteTests(testValues, i => i);
+    }
+
+    [DayTest]
+    public TestResult Test() => ExecuteTest((79, 3621), () => Merge(testFields));
+
+    [DayPart]
+    public string Solve()
+    {
+        var (beacons, largestDistance) = Merge(partsFields);
+
+        return $"{beacons} {largestDistance}";
     }
 }
