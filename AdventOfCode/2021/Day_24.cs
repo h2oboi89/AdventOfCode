5cd1d8595a1b7155e7ef2f22b71b834d00b44170
Hotel2Oscar <waters.daniel.c@gmail.com>
2021-12-24 09:39:53 -0500

    2021.24 files

---
+++ AdventOfCode/2021/Day_24.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2021;
+
+internal class Day_24 : BaseDay
+{
+    public Day_24(string inputFile)
+    {
+
+    }
+}
