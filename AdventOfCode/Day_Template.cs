507ce70c0bb248705a5ec081911780acc801eebd
Waters <h2oboi89@gmail.com>
2022-12-19 22:52:38 -0500

    added "YEAR" to day template namespace

--- AdventOfCode/Day_Template.cs
+++ AdventOfCode/Day_Template.cs
@@ -5,7 +5,7 @@ using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AdventOfCode
+namespace AdventOfCode.YEAR
 {
     internal class Day_Template : BaseDay
     {
